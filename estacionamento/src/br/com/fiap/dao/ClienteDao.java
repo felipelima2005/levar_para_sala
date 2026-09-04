895b090cc677c020abc4fe7116befa883266f127
felipelima2005 <felipelimaoliveira2008@gmail.com>
2026-09-04 11:03:24 -0300

    feat: Cliente e ClienteDao Criados e já estruturados

---
+++ estacionamento/src/br/com/fiap/dao/ClienteDao.java
@@ -0,0 +1,92 @@
+package br.com.fiap.dao;
+
+import br.com.fiap.dto.Carro;
+import br.com.fiap.dto.Cliente;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ClienteDao {
+    private Connection con;
+
+    public ClienteDao(Connection con) {
+        this.con = con;
+    }
+
+    public Connection getCon() {
+        return con;
+    }
+
+    public  String inserir(Cliente cliente){
+        String sql = "insert into ddd_cliente(id_cliente,nome_cliente,placa) values(?,?,?)";
+        try(PreparedStatement ps = getCon().prepareStatement(sql)) {
+            ps.setInt(1, cliente.getIdCliente());
+            ps.setString(2, cliente.getNomeCliente());
+            ps.setString(3, cliente.getPlaca());
+            if (ps.executeUpdate() > 0) {
+                return "inserido com sucesso";
+            }else {
+                return "Erro ao inserir";
+            }
+        } catch (SQLException e) {
+            return "Erro de SQL" + e.getMessage();
+        }
+    }
+
+    public  String alterar(Cliente cliente){
+        String sql = "update ddd_cliete set nomeCliente=?, placa=? where idCliente=?";
+        try(PreparedStatement ps = getCon().prepareStatement(sql)) {
+            ps.setString(1, cliente.getNomeCliente());
+            ps.setString(2, cliente.getPlaca());
+            ps.setInt(3, cliente.getIdCliente());
+            if (ps.executeUpdate() > 0) {
+                return "Alterado com sucesso";
+            }else {
+                return "Erro ao alterar";
+            }
+        } catch (SQLException e) {
+            return "Erro de SQL" + e.getMessage();
+        }
+    }
+
+    public  String excluir(Cliente cliente){
+        String sql = "delete from ddd_cliente where idCliente=?";
+        try(PreparedStatement ps = getCon().prepareStatement(sql)) {
+            ps.setInt(1, cliente.getIdCliente());
+            if (ps.executeUpdate() > 0) {
+                return "Excluido com sucesso";
+            }else {
+                return "Erro ao Excluir";
+            }
+        } catch (SQLException e) {
+            return "Erro de SQL" + e.getMessage();
+        }
+    }
+
+    public ArrayList<Cliente> listarTodos(){
+        String sql = "select * from ddd_cliente order by idCliente";
+        ArrayList<Cliente> listarCliente = new ArrayList<>();
+        try(PreparedStatement ps = getCon().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery()) {
+            if (rs != null) {
+                while (rs.next()){
+                    Cliente cliente = new Cliente();
+                    cliente.setIdCliente(rs.getInt(1));
+                    cliente.setNomeCliente(rs.getString(2));
+                    cliente.setPlaca(rs.getString(3));
+                    listarCliente.add(cliente);
+                }
+                return listarCliente;
+            }else{
+                return null;
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro de SQL" + e.getMessage());;
+            return null;
+        }
+
+    }
+}
